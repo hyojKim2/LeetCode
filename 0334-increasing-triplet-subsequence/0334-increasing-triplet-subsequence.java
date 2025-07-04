d201a87a74c7c2350959e84e8e765d2e9a3bbb0b
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-07-04 14:43:34 +0900

    Time: 2 ms (96.87%), Space: 133.9 MB (52.98%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,41 +1,14 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-
-        int ex=nums[0];
-
-        int cnt=1;
-
-        // 중복 제거용 Set
-        Set<Integer> uniqueSet = new HashSet<>();
-
-        // 배열 요소를 Set에 추가
-        for (int num : nums) {
-            uniqueSet.add(num);
-        }
-        if(uniqueSet.size()==2){
-            return false;
-        }
-
-        for(int i=1; i< nums.length-1; i++){
-            cnt=1;
-            ex=nums[i-1];
-            for(int j=i; j<nums.length ; j++){
-                if(ex>nums[j] && nums[j]>nums[i-1]){
-                    ex=nums[j];
-                }
-                
-                if(ex < nums[j]){
-                    cnt++;
-                    ex=nums[j];
-                }
-                
-                if(cnt==3){
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        int small = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
         
+        // 두 개의 요소로 계산
+        for(int e : nums){
+            if(e <= small) small = e;
+            else if(e <= second) second = e;
+            else return true; 
+        }                
+        return false;
     }
 }
+
