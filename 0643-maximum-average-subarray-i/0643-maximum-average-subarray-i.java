c091f1c88350eeb50545e90188455cd331dd04a7
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-08-07 20:10:53 +0900

    Time: 2 ms (99.88%), Space: 56.3 MB (65.43%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,28 +1,20 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
 
-        // 이전 윈도우와, 현재 윈도우의 양끝 변동값 확인해서 불필요한 계산 스킵 가능
-
-
+        // 슬라이딩 윈도우: 이전 윈도우와, 현재 윈도우의 양끝 변동값 확인해서 불필요한 계산 스킵 가능
         int max =0;
         for(int i=0;i<k;i++){
             max+=nums[i];
         }
-        int prev = max;
-
+        int sum = max;
 
 
         for(int i =1 ; i <= nums.length - k; i++ ){
              
-            //prev 구하기. 
-            if( nums[i-1]   <=  nums[i+ k-1]){
-                prev=0;
-                for(int j = i ; j<i +k ; j++){
-                    prev+=nums[j];
-                }
-            }
-            if(prev > max ){
-                max=prev;
+            // 이동하며 값 갱신. 
+            sum = sum-nums[i-1] +nums[i+ k-1];
+            if(sum > max ){
+                max=sum;
             }
 
         }
