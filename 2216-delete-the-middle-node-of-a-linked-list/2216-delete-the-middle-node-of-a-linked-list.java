9e6b66027cd502b2e47618fff4e13a7b9c10a775
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-09-27 02:01:05 +0900

    Time: 13 ms (1.23%), Space: 67.9 MB (5.52%) - LeetHub

---
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+
+        /**
+        먼저 연결리스트 전체 크기를 구한다. 
+         */
+
+        int midIdx=0;
+        int size=0;
+        ListNode curr=head;
+
+        while(curr!=null){
+            curr=curr.next;
+            size++;
+        }
+        
+        midIdx=size/2;
+
+        System.out.println(midIdx);
+
+        ListNode sol= null;
+        curr=head;
+        int cnt =0;
+
+        if(midIdx==0){
+            return null;
+        }
+        while(curr!=null){
+            if(cnt==midIdx-1){
+                curr.next=curr.next.next;
+                if(curr.next!=null){
+                    System.out.println(curr.val + " " + curr.next.val );
+                }
+            }
+
+            ListNode temp=curr;
+            curr=curr.next;
+
+            cnt++;
+            
+        }
+
+
+        return head;
+        
+    }
+}
