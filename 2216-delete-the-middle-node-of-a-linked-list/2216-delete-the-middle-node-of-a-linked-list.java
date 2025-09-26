c2e967520b31db93b956ed1736580b6c277aae09
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-09-27 02:10:00 +0900

    Time: 3 ms (99.5%), Space: 63.1 MB (82.27%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -12,39 +12,70 @@ class Solution {
     public ListNode deleteMiddle(ListNode head) {
 
         /**
-        먼저 연결리스트 전체 크기를 구한다. 
-         */
+        2개의 인덱스 slow, fast를 두고 각각 1칸, 2칸씩 이동하기 */
 
-        int midIdx=0;
-        int size=0;
-        ListNode curr=head;
+        // 
+        if(head==null || head.next==null){
+            return null;
 
-        while(curr!=null){
-            curr=curr.next;
-            size++;
+        }
+
+        //size가 2
+        if(head.next.next==null){
+            head.next=null;
+            return head;
         }
         
-        midIdx=size/2;
 
-        curr=head;
-        int cnt =0;
+        //slow, fast
+        ListNode slow=head, fast= head;
 
-        if(midIdx==0){
-            return null;
-        }
-        while(curr!=null){
-            if(cnt==midIdx-1){
-                curr.next=curr.next.next;
-            }
+        while(fast!=null && fast.next!=null ){
+            slow=slow.next;
+            fast=fast.next.next;
 
-            curr=curr.next;
-
-            cnt++;
-            
         }
 
+        slow.val=slow.next.val;
+        slow.next=slow.next.next;
 
         return head;
+
+
+        // /**
+        // 먼저 연결리스트 전체 크기를 구한다. 
+        //  */
+
+        // int midIdx=0;
+        // int size=0;
+        // ListNode curr=head;
+
+        // while(curr!=null){
+        //     curr=curr.next;
+        //     size++;
+        // }
+        
+        // midIdx=size/2;
+
+        // curr=head;
+        // int cnt =0;
+
+        // if(midIdx==0){
+        //     return null;
+        // }
+        // while(curr!=null){
+        //     if(cnt==midIdx-1){
+        //         curr.next=curr.next.next;
+        //     }
+
+        //     curr=curr.next;
+
+        //     cnt++;
+            
+        // }
+
+
+        // return head;
         
     }
 }
