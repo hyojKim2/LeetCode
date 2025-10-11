170ce1cfa3644f960b9ca71568e18d3f71f2453c
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-10-11 14:55:44 +0900

    Time: 17 ms (47.81%), Space: 43 MB (92.54%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -13,29 +13,34 @@
  *     }
  * }
  */
-
-import java.util.HashMap;
-
 class Solution {
+    int target;
+    int answer=0;
     public int pathSum(TreeNode root, int targetSum) {
-        HashMap<Long, Integer> prefixSum = new HashMap<>();
-        prefixSum.put(0L, 1); // 누적합이 targetSum과 같을 때를 위해 초기값
-        return dfs(root, 0L, targetSum, prefixSum);
+        target=targetSum;
+
+        traverse(root);
+        return answer;
     }
-    
-    private int dfs(TreeNode node, long currSum, int target, HashMap<Long, Integer> prefixSum) {
-        if (node == null) return 0;
-        
-        currSum += node.val;
-        int count = prefixSum.getOrDefault(currSum - target, 0); // 이전 누적합과 비교
-        
-        prefixSum.put(currSum, prefixSum.getOrDefault(currSum, 0) + 1);
-        
-        count += dfs(node.left, currSum, target, prefixSum);
-        count += dfs(node.right, currSum, target, prefixSum);
-        
-        prefixSum.put(currSum, prefixSum.get(currSum) - 1); // 백트래킹
-        
-        return count;
+
+    //현재 노드를 시작점으로 
+    public void traverse(TreeNode root){
+        if(root==null) return;
+        DFS(root, 0);
+        traverse(root.left);
+        traverse(root.right);
+    }
+
+    public void DFS(TreeNode root, long sum){
+
+        if(root==null) return ;
+
+        sum+=root.val;
+        if(sum==target){
+            answer++;
+        }
+        DFS(root.left, sum);
+        DFS(root.right, sum);
+
     }
 }
