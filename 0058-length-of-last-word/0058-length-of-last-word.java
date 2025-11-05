8e69be309bad003c2caa4ba988b5a9fce4bc7964
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-11-05 17:37:02 +0900

    Time: 1 ms (27.64%), Space: 43 MB (5.77%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        String[] strs = s.split(" ");
+        return strs[strs.length -1 ].length();        
+    }
+}
