426627b79d66235e8fcc0caa57d0e3e35ccd901e
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-11-22 16:39:57 +0900

    Time: 49 ms (19.93%), Space: 146.8 MB (5.38%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -11,12 +11,19 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int spell = spells[i];
 
-            // 최소로 필요한 포션의 strength
-            // spell * potion >= success → potion >= success / spell
-            long needed = (success + spell - 1) / spell; // 올림 나눗셈
-
-            // 이분 탐색으로 potions에서 needed 이상인 첫 위치 찾기
-            int idx = lowerBound(potions, needed);
+            int left =0;
+            int right= m-1;
+            int idx=m;
+            while(left<=right){
+                int mid = (left+right) /2;
+                if( (long) spell  * potions[mid] >=success){
+                    idx=mid;
+                    right=mid-1;
+                }
+                else {
+                    left =mid+1;
+                }
+            }
 
             // 성공하는 포션 개수 = 전체 - idx
             result[i] = m - idx;
@@ -25,22 +32,4 @@ class Solution {
         return result;
     }
 
-    // lowerBound: 배열에서 target 이상이 처음 등장하는 index
-    private int lowerBound(int[] arr, long target) {
-        int left = 0, right = arr.length - 1;
-        int pos = arr.length;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] >= target) {
-                pos = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return pos;
-    }
 }
