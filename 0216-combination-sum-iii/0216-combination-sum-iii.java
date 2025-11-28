e9d1dabf34b82c6344cdbb5e47dbf9581607d5f1
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2025-11-28 21:32:20 +0900

    Time: 1 ms (20.95%), Space: 42.4 MB (17%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+
+class Solution {
+    List<List<Integer>> answer;
+    public List<List<Integer>> combinationSum3(int k, int n) {
+
+        //k개의 숫자의 합이 n인 것을 찾는다. 
+        // 2<= k <=9
+
+        answer= new ArrayList<>();
+
+        dfs(1, 0, new ArrayList<>(), n, k);
+
+        return answer;
+
+        
+    }
+
+    public void dfs(int start, int sum, List<Integer> list,  int ans, int target){
+        
+        if(list.size()==target){
+            if(sum==ans && !answer.contains(list)) {
+                answer.add(new ArrayList<>(list));
+            }
+            return;            
+        }
+
+
+        for(int i=start;i<=9;i++){
+            if(!list.contains(i)){ //사용되지 않은 자연수만
+                list.add(i);
+                sum+=i;
+                dfs(i+1, sum, list, ans, target);
+                sum-=i;
+                list.remove(list.size()-1);
+            }
+        }
+
+
+        return;
+
+    }
+
+    
+}
