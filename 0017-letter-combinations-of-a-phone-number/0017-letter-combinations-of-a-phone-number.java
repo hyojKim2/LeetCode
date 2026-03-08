66875984c2f76d43e7ad758f4bf449f0219048fb
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2026-03-08 19:27:35 +0900

    Time: 0 ms (100%), Space: 43.6 MB (63.7%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,48 +1,42 @@
 import java.util.*;
 
 class Solution {
-    List<String> answer;
-    Map<Integer, List<Character> >  map ;
+
+    private static final String[] map = {
+        "", "", "abc", "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+
     public List<String> letterCombinations(String digits) {
-        answer =  new ArrayList<>();
-        map = new HashMap<>();
-        map.put(2, Arrays.asList('a', 'b', 'c'));
-        map.put(3, Arrays.asList('d', 'e', 'f'));
-        map.put(4, Arrays.asList('g', 'h', 'i'));
-        map.put(5, Arrays.asList('j', 'k', 'l'));
-        map.put(6, Arrays.asList('m', 'n', 'o'));
-        map.put(7, Arrays.asList('p', 'q', 'r', 's'));
-        map.put(8, Arrays.asList('t', 'u', 'v'));
-        map.put(9, Arrays.asList('w', 'x', 'y', 'z'));
 
+        List<String> result = new ArrayList<>();
 
-        int[] arr = digits.chars().map(c-> c-'0').toArray();
-        
-        dfs(0, arr, new StringBuilder());
+        if (digits.length() == 0) return result;
 
+        dfs(digits, 0, new StringBuilder(), result);
 
-        return answer;
-        
+        return result;
     }
 
-    public void dfs(int index, int[]arr, StringBuilder sb){
+    private void dfs(String digits, int index, StringBuilder path, List<String> result) {
 
-        // 다 만들었으면 추가하기 
-        if(index==arr.length){
-            answer.add(sb.toString());
+        if (index == digits.length()) {
+            result.add(path.toString());
             return;
         }
 
-        // 현재 차례 번호의 문자 가져오기 
-        int digit=arr[index];
-        List<Character> list = map.get(digit);
+        String letters = map[digits.charAt(index) - '0']; // 아스키코드 mm- 48 빼준 결과. 
 
-        // 번호가 가진 문자 기준 빌딩
-        for(char ch:list){
-            sb.append(ch);
-            dfs(index +1, arr, sb); // 다음 인덱스로 넘어감. 
-            sb.deleteCharAt(sb.length()-1); // backtrack;''
+        for (char c : letters.toCharArray()) {
+
+            path.append(c);
+
+            dfs(digits, index+1, path, result );
+
+            //backtracking
+            path.deleteCharAt(path.length()-1);
+
         }
-    }
 
+    }
 }
