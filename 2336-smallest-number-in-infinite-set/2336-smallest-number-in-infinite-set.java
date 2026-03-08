c6e381b11db2ce50f08678439a6c78f4794d065d
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2026-03-08 18:45:35 +0900

    Time: 10 ms (88.76%), Space: 46.8 MB (97%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
@@ -1,32 +1,36 @@
 class SmallestInfiniteSet {
 
-    private static PriorityQueue<Integer> heap;
+    private PriorityQueue<Integer> heap;
+    private HashSet<Integer> set;
+    private int current; // 1~1000이 들은 것처럼 가정하는 변수 
 
     //초기화. 양수를 갖는 셋을 초기화
     public SmallestInfiniteSet() {
         
         heap = new PriorityQueue<>();
+        set =new HashSet<>();
+        current = 1;
         
-        for(int i=1;i<=1000;i++){
-            heap.add(i);
-        }
 
     }
     
     // 가장 작은 수를 없앤다. -> 최소힙!
     public int popSmallest() {
+        if(!heap.isEmpty()){
+            set.remove(heap.peek());
+            return heap.poll();
+        }
         
-        return heap.poll();
-        
+        return current++;
     }
     
     // 수 추가 
     public void addBack(int num) {
 
-        if(heap.contains(num)){
-            return;
+        if(num < current && !set.contains(num)){
+            set.add(num);
+            heap.add(num);
         }
-        heap.add(num);
     }
 }
 
